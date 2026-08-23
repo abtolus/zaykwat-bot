194dae774be8668fb239f296dbd4a572627db88c
Kaung Set Nyunt <301109589+abtolus@users.noreply.github.com>
2026-08-23 21:36:00 +0630

    feat: add foreign exchange rates

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import os
 import telebot
+import requests
+import datetime
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -16,6 +18,7 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.reply_to(message, message.text, reply_markup=main_menu())
+
 def main_menu():
     markup = InlineKeyboardMarkup()
     currencyExchangesButton = InlineKeyboardButton("Currency Exchanges", callback_data="currencyExchangesButton")
@@ -25,9 +28,50 @@ def main_menu():
     markup.row(currencyExchangesButton, refreshButton)
     markup.row(fuelPricesButton, goldPricesButton)
     return markup
+def get_currency_exchanges():
+    url = os.getenv('CURRENCY_EXCHANGES_API_URL')
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+
+        data = response.json()
+        if data.get('result') != "success":
+            return "Unable to fetch Currency Exchanges at the moment."
+
+        rates = data["rates"]
+        USDToMMK = rates.get('MMK', 0)
+        targets = ["USD", "EUR", "SGD", "MYR", "CNY", "THB", "JPY"]
+        bodyMessage = f"```text\nနိုင်ငံခြားငွေလဲလှယ်နှုန်း\n\n{'ငွေကြေး':<10} {'ဈေးနှုန်း':<5}\n{'-' * 16}\n"
+
+        for target in targets:
+            if target == "USD":
+                MMK = USDToMMK
+            else:
+                rateInUSD = rates.get(target)
+                MMK = (USDToMMK / rateInUSD) if rateInUSD else 0
+            bodyMessage += f"{target:<10} {MMK:<5,.0f}\n"
+
+        bodyMessage += "```\n"
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        footerMessage = now
+        return bodyMessage + footerMessage
+
+    except requests.exceptions.RequestException:
+        return "Error connecting to the Currency Exchanges service."
+
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-    if call.data == "refreshButton":
+    if call.data == "currencyExchangesButton":
+        bot.answer_callback_query(call.id, text="Fetching Currency Exchanges...")
+        message = get_currency_exchanges()
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=message,
+            parse_mode="Markdown",
+            reply_markup=main_menu()
+        )
+    elif call.data == "refreshButton":
         bot.answer_callback_query(call.id, text="Refreshed.")
     else:
         bot.answer_callback_query(call.id, text="Coming soon.")
